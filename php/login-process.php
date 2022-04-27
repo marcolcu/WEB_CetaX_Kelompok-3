15881af65a20a483c45e77e201929259ca0b0c22
George <91233711+Geor03@users.noreply.github.com>
2022-04-27 20:06:30 +0700

    as

--- php/login-process.php
+++ php/login-process.php
@@ -4,12 +4,16 @@ session_start();
 $username = $_POST['user'];
 $password = $_POST['pw'];
 
-$k = new PDO();
+$host = 'localhost';
+$dbname = 'cetax';
+$username = 'root';
+$password = '';
+$pdo = new PDO("mysql: host=$host;dbname=$dbname",$username,$password);
+$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
-$sql = "SELECT * FORM users
-        WHERE username = ?";
+$sql = "SELECT * FROM users WHERE username = ?";
 
-$result = $k->prepare($sql);
+$result = $pdo->prepare($sql);
 $result->execute([$username]);
 
 if($row = $result->fetch()) {
